1f0818c39b701a5fe3fdf325d196dbe31aa6906b
killerber4t <t.caliskan@live.de>
2020-03-03 13:33:45 +0100

    add spring stuff

---
+++ gruppen2/src/main/java/mops/gruppen2/Gruppen2Application.java
@@ -0,0 +1,13 @@
+package mops.gruppen2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppen2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gruppen2Application.class, args);
+	}
+
+}
---
+++ gruppen2/src/test/java/mops/gruppen2/Gruppen2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mops.gruppen2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppen2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
