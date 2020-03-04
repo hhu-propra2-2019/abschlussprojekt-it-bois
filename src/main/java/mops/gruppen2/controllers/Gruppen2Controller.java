6a3483db9d8472c6e81f24ffc413900c75aeeba8
XXNitram <matti.55@hotmail.de>
2020-03-04 13:56:12 +0100

    add RequestMapping

--- src/main/java/mops/gruppen2/controllers/Gruppen2Controller.java
+++ src/main/java/mops/gruppen2/controllers/Gruppen2Controller.java
@@ -2,7 +2,9 @@ package mops.gruppen2.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/gruppen2")
 @Controller
 public class Gruppen2Controller {
     @GetMapping("/")
