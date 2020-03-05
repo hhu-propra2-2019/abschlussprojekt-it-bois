3bc6f76b65e6dcee7d9da8d27b079be1da4df512
tomvahl <tom-vahl@hotmail.com>
2020-03-05 13:55:34 +0100

    add DTOs

---
+++ src/main/java/mops/gruppen2/entities/GruppeDTO.java
@@ -0,0 +1,6 @@
+package mops.gruppen2.entities;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GruppeDTO extends CrudRepository<Gruppe,Long> {
+}
---
+++ src/main/java/mops/gruppen2/entities/TeilnehmerDTO.java
@@ -0,0 +1,6 @@
+package mops.gruppen2.entities;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeilnehmerDTO extends CrudRepository<Teilnehmer,Long> {
+}
