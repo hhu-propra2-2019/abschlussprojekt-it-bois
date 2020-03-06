d14d95dad939f13e80e12aef1c92d166de6f3b4e
Christoph <tobi@urpost.de>
2020-03-06 16:21:20 +0100

    add assertJ + UpdateRoleEvent test

--- src/test/java/mops/gruppen2/entities/GruppeTest.java
+++ src/test/java/mops/gruppen2/entities/GruppeTest.java
@@ -2,7 +2,7 @@ package mops.gruppen2.entities;
 
 import mops.gruppen2.events.AddUser;
 import mops.gruppen2.events.CreateGroupEvent;
-import mops.gruppen2.events.Event;
+import mops.gruppen2.events.UpdateRoleEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 class GruppeTest {
@@ -47,5 +48,24 @@ class GruppeTest {
         testGruppe.setTeilnehmersList(testTeil);
 
         assertEquals(testGruppe,gruppe);
+    }
+
+    // Verwendet CreateGroupEvent und AddUserEvent
+    @Test
+    void updateRoleForExistingUser() {
+        // Arrange
+        Gruppe gruppe = new Gruppe();
+
+        gruppe.applyEvent(new CreateGroupEvent(1L, 1L, "1L", "gruppe1", "Eine Testgruppe"));
+        gruppe.applyEvent(new AddUser(1L, 1L, "5L", "Peter", "Pan", "123@mail.de"));
+
+
+        // Act
+        gruppe.applyEvent(new UpdateRoleEvent(1L, 1L, "1L", new Orga()));
+
+        // Assert
+        assertThat(gruppe.getRollenList())
+                .containsOnlyKeys(gruppe.getTeilnehmersList().get(0))
+                .containsValue(new Orga());
     }
 }
