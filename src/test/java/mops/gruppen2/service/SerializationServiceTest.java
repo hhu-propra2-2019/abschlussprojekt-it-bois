1eeda87c7f6e7b5b2134ccd3068ec5bf41fe5fb8
Christoph <tobi@urpost.de>
2020-03-09 16:48:54 +0100

    Change Lombok annotations to enable Jackson deserializing

--- src/test/java/mops/gruppen2/service/SerializationServiceTest.java
+++ src/test/java/mops/gruppen2/service/SerializationServiceTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class SerializationServiceTest {
 
