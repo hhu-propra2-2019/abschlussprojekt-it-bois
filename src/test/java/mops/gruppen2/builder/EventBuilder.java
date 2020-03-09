1b1de974c6b79c64f17e69785a53ccc93a7142eb
Christoph <tobi@urpost.de>
2020-03-09 15:49:20 +0100

    Add class to build random events for easier testing

---
+++ src/test/java/mops/gruppen2/builder/EventBuilder.java
@@ -0,0 +1,92 @@
+package mops.gruppen2.builder;
+
+import com.github.javafaker.Faker;
+import mops.gruppen2.domain.Role;
+import mops.gruppen2.domain.event.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventBuilder {
+
+    public static CreateGroupEvent randomCreateGroupEvent() {
+        Faker faker = new Faker();
+
+        return new CreateGroupEvent(
+                faker.random().nextLong(),
+                faker.random().nextLong(),
+                faker.random().hex(),
+                faker.leagueOfLegends().champion(),
+                faker.leagueOfLegends().quote()
+        );
+    }
+
+    public static AddUserEvent randomAddUserEvent() {
+        Faker faker = new Faker();
+
+        String firstname = faker.name().firstName();
+        String lastname = faker.name().lastName();
+
+        return new AddUserEvent(
+                faker.random().nextLong(),
+                faker.random().nextLong(),
+                faker.random().hex(),
+                firstname,
+                lastname,
+                firstname + "." + lastname + "@mail.de"
+        );
+    }
+
+    public static List<Event> randomAddUserEvents(int count) {
+        List<Event> eventList = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            eventList.add(EventBuilder.randomAddUserEvent());
+        }
+
+        return eventList;
+    }
+
+    public static DeleteUserEvent randomDeleteUserEvent(long group_id, String user_id) {
+        Faker faker = new Faker();
+
+        return new DeleteUserEvent(
+                faker.random().nextLong(),
+                group_id,
+                user_id
+        );
+    }
+
+    public static UpdateGroupDescriptionEvent randomUpdateGroupDescriptionEvent(long group_id) {
+        Faker faker = new Faker();
+
+        return new UpdateGroupDescriptionEvent(
+                faker.random().nextLong(),
+                group_id,
+                faker.random().hex(),
+                faker.leagueOfLegends().quote()
+        );
+    }
+
+    public static UpdateGroupTitleEvent randomUpdateGroupTitleEvent(long group_id) {
+        Faker faker = new Faker();
+
+        return new UpdateGroupTitleEvent(
+                faker.random().nextLong(),
+                group_id,
+                faker.random().hex(),
+                faker.leagueOfLegends().champion()
+        );
+    }
+
+    public static UpdateRoleEvent randomUpdateRoleEvent(long group_id, String user_id, Role role) {
+        Faker faker = new Faker();
+
+        return new UpdateRoleEvent(
+                faker.random().nextLong(),
+                group_id,
+                user_id,
+                role
+        );
+    }
+}
