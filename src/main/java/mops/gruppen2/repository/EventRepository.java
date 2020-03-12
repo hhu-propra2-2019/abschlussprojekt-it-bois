c6c62b07bfa1f5e11739dd05b6f01302be7304c5
LukasEttel <lukas.ettie@yahoo.de>
2020-03-12 13:52:50 +0100

    created first API implementation

--- src/main/java/mops/gruppen2/repository/EventRepository.java
+++ src/main/java/mops/gruppen2/repository/EventRepository.java
@@ -1,9 +1,14 @@
 package mops.gruppen2.repository;
 
 import mops.gruppen2.domain.EventDTO;
+import mops.gruppen2.domain.event.Event;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends CrudRepository<EventDTO, Long> {
+    @Query("SELECT * FROM event WHERE event_id > ?#{[0]}")
+    public Iterable<EventDTO> findNewEventSinceStatus(@Param("status") Long status);
 }
