6b788beb4b4e0a92b652b330ac08c1bb19870bd3
XXNitram <matti.55@hotmail.de>
2020-03-13 13:46:34 +0100

    Refactor ControllerService and add an UpdateRoleEvent to createGroup

--- src/main/java/mops/gruppen2/service/EventService.java
+++ src/main/java/mops/gruppen2/service/EventService.java
@@ -94,4 +94,15 @@ public class EventService {
         return events;
     }
 
+    /**
+     * Sichert eine Liste von Event Objekten mithilfe der Methode saveEvent(Event event)
+     *
+     * @param createGroupEvents Liste von Event Objekten
+     */
+    public void saveEventList(List<Event> createGroupEvents) {
+        for(Event event : createGroupEvents) {
+            saveEvent(event);
+        }
+    }
+
 }
