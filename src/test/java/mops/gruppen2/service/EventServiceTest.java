061d6d2390476cd5a8887ad8366aa6e923cac07a
Mahgs <mahgs@gmx.de>
2020-03-16 13:15:54 +0100

    merged with master

--- src/test/java/mops/gruppen2/service/EventServiceTest.java
+++ src/test/java/mops/gruppen2/service/EventServiceTest.java
@@ -5,11 +5,6 @@ import mops.gruppen2.domain.GroupType;
 import mops.gruppen2.domain.Visibility;
 import mops.gruppen2.domain.event.AddUserEvent;
 import mops.gruppen2.domain.event.CreateGroupEvent;
-import mops.gruppen2.domain.event.Event;
-import mops.gruppen2.domain.GroupType;
-import mops.gruppen2.domain.Visibility;
-import mops.gruppen2.domain.event.AddUserEvent;
-import mops.gruppen2.domain.event.CreateGroupEvent;
 import mops.gruppen2.repository.EventRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -55,39 +50,6 @@ class EventServiceTest {
         assertEquals(eventService.getMaxEvent_id(), 42L);
     }
 
-    //Funktionert nicht
-    @Test
-    void getEventListAfterStatus() {
-        List<Long> eventIDs = new ArrayList<>();
-        eventIDs.add(9L);
-        eventIDs.add(10L);
-        when(eventRepositoryMock.findNewEventSinceStatus(8L)).thenReturn(eventIDs);
-
-        List<EventDTO> eventDTOs = new ArrayList<>();
-        EventDTO eventDTO1 = new EventDTO();
-        eventDTO1.setEvent_id(9L);
-        eventDTO1.setGroup_id(1L);
-        eventDTO1.setUser_id("ad");
-        eventDTO1.setEvent_payload({"type":"CreateGroupEvent","event_id":null,"group_id":1,"user_id":"orga","groupVisibility":"PUBLIC","groupParent":null,"groupType":"LECTURE"} );
-        eventDTOs.add(eventDTO1);
-        EventDTO eventDTO2 = new EventDTO();
-        eventDTO2.setEvent_id(10L);
-        eventDTO2.setGroup_id(1L);
-        eventDTO2.setUser_id("sd");
-        eventDTOs.add(eventDTO2);
-        when(eventRepositoryMock.findAllEventsOfGroups(eventIDs)).thenReturn(eventDTOs);
-
-
-        List<Event> newEvents = new ArrayList<>();
-        CreateGroupEvent event1 = new CreateGroupEvent(1L, "user1", 1L, GroupType.LECTURE, Visibility.PRIVATE);
-        newEvents.add(event1);
-        event1.setEvent_id(9L);
-        CreateGroupEvent event2 = new CreateGroupEvent(2L, "user2", 1L, GroupType.SIMPLE, Visibility.PRIVATE);
-        event2.setEvent_id(57L);
-        newEvents.add(event2);
-        //assertEquals(eventService.getNewEvents(8L), newEvents);
-    }
-
     @Test
     void checkGroupReturnNextValue() {
         List<EventDTO> eventDTOS = new ArrayList<>();
@@ -111,15 +73,15 @@ class EventServiceTest {
     }
 
     @Test
-    void getDTOOffentlichTest(){
-        CreateGroupEvent createGroupEvent = new CreateGroupEvent(eventService.checkGroup(), "test", null , GroupType.LECTURE, Visibility.PUBLIC);
+    void getDTOOffentlichTest() {
+        CreateGroupEvent createGroupEvent = new CreateGroupEvent(eventService.checkGroup(), "test", null, GroupType.LECTURE, Visibility.PUBLIC);
         EventDTO eventDTO = eventService.getDTO(createGroupEvent);
         assertEquals(eventDTO.isVisibility(), true);
     }
 
     @Test
-    void getDTOPrivatTest(){
-        AddUserEvent addUserEvent = new AddUserEvent(eventService.checkGroup(), "test","franz","mueller","a@a");
+    void getDTOPrivatTest() {
+        AddUserEvent addUserEvent = new AddUserEvent(eventService.checkGroup(), "test", "franz", "mueller", "a@a");
         EventDTO eventDTO = eventService.getDTO(addUserEvent);
         assertEquals(eventDTO.isVisibility(), false);
     }
