a6667e40915d1ca5eb17d3a307f8da923661d599
killerber4t <t.caliskan@live.de>
2020-03-18 13:08:35 +0100

    fix text overflow in groupdescription

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE event
     event_id INT PRIMARY KEY AUTO_INCREMENT,
     group_id INT NOT NULL,
     user_id VARCHAR(50),
-    event_payload VARCHAR(255),
+    event_payload VARCHAR(2500),
     visibility BOOLEAN
 );
