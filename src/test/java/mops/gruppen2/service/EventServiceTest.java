a307476b07b5221bfd70c9db787fa52f6ce07e48
killerber4t <t.caliskan@live.de>
2020-03-24 13:43:36 +0100

    fix JsonService implementation

--- src/test/java/mops/gruppen2/service/EventServiceTest.java
+++ src/test/java/mops/gruppen2/service/EventServiceTest.java
@@ -1,13 +1,9 @@
 package mops.gruppen2.service;
 
 import mops.gruppen2.Gruppen2Application;
-import mops.gruppen2.domain.GroupType;
-import mops.gruppen2.domain.Visibility;
 import mops.gruppen2.domain.dto.EventDTO;
-import mops.gruppen2.domain.event.AddUserEvent;
 import mops.gruppen2.domain.event.CreateGroupEvent;
 import mops.gruppen2.domain.event.Event;
-import mops.gruppen2.domain.event.UpdateGroupTitleEvent;
 import mops.gruppen2.repository.EventRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,9 +16,13 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.ARRAY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -38,11 +38,13 @@ class EventServiceTest {
 
     @Autowired
     private EventRepository eventRepository;
+    @Autowired
+    private JsonService jsonService;
     private EventService eventService;
 
     @BeforeEach
     void setUp() {
-        eventService = new EventService(mock(JsonService.class), eventRepository);
+        eventService = new EventService(jsonService, eventRepository);
     }
 
     @Test
@@ -69,9 +71,8 @@ class EventServiceTest {
         List<EventDTO> eventDTOS1 = new ArrayList<>();
         eventDTOS1.add(eventDTO1);
         eventDTOS1.add(eventDTO2);
-        Iterable<EventDTO> eventDTOS2 = eventDTOS1;
-        List<Event> events = eventService.translateEventDTOs(eventDTOS2);
-        assertTrue(events.get(0).getClass().isInstance(CreateGroupEvent.class));
+        List<Event> events = eventService.translateEventDTOs(eventDTOS1);
+        assertThat(events.get(0)).isInstanceOf(CreateGroupEvent.class);
     }
 
 }
