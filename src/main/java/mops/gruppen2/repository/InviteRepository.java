f581169afc8c91025af5bf5508627985fe39cc70
Christoph <tobi@urpost.de>
2020-03-26 16:45:54 +0100

    queries
    Co-authored-by: [Mahgs] <maxoerter@gmx.de>
    Co-authored-by: Christoph <tobi@urpost.de>

--- src/main/java/mops/gruppen2/repository/InviteRepository.java
+++ src/main/java/mops/gruppen2/repository/InviteRepository.java
@@ -3,9 +3,13 @@ package mops.gruppen2.repository;
 import mops.gruppen2.domain.dto.InviteLinkDTO;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface InviteRepository extends CrudRepository<InviteLinkDTO, Long> {
 
-    @Query("")
-    String ggg();
+    @Query("SELECT group_id FROM invite WHERE invite_link = :link")
+    String findGroupIdByLink(@Param("link") String link);
+
+    @Query("DELETE FROM invite WHERE group_id = :group")
+    void deleteLinkOfGroup(@Param("group") String group);
 }
