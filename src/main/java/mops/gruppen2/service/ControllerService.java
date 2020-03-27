9a0035ba1a3e8fbde045790bbf8e34aab34e5fae
[Mahgs] <maxoerter@gmx.de>
2020-03-27 15:58:39 +0100

    added javaDoc Comments to ControllerService

--- src/main/java/mops/gruppen2/service/ControllerService.java
+++ src/main/java/mops/gruppen2/service/ControllerService.java
@@ -1,6 +1,5 @@
 package mops.gruppen2.service;
 
-import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import mops.gruppen2.domain.Group;
 import mops.gruppen2.domain.GroupType;
 import mops.gruppen2.domain.Role;
@@ -21,7 +20,6 @@ import mops.gruppen2.security.Account;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.CharConversionException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -78,7 +76,20 @@ public class ControllerService {
         return groupId;
     }
 
-    public void createGroupAsOrga(Account account, String title, String description, Boolean isVisibilityPrivate, Boolean isLecture, Boolean isMaximumInfinite, Long userMaximum, UUID parent, MultipartFile file) throws EventException {
+    /**
+     * Wie createGroup, nur das hier die Gruppe auch als Veranstaltung gesetzt werden kann und CSV Dateien mit Nutzern
+     * eingelesen werden können.
+     * @param account Der Nutzer der die Gruppe erstellt
+     * @param title Parameter für die neue Gruppe
+     * @param description Parameter für die neue Gruppe
+     * @param isVisibilityPrivate Parameter für die neue Gruppe
+     * @param isLecture Parameter für die neue Gruppe
+     * @param isMaximumInfinite Parameter für die neue Gruppe
+     * @param userMaximum Parameter für die neue Gruppe
+     * @param parent Parameter für die neue Gruppe
+     * @param file Parameter für die neue Gruppe
+     */
+    public void createGroupAsOrga(Account account, String title, String description, Boolean isVisibilityPrivate, Boolean isLecture, Boolean isMaximumInfinite, Long userMaximum, UUID parent, MultipartFile file) {
         userMaximum = checkInfiniteUsers(isMaximumInfinite, userMaximum);
 
         List<User> newUsers = readCsvFile(file);
@@ -102,7 +113,13 @@ public class ControllerService {
         }
     }
 
-
+    /**
+     * Wenn die maximale Useranzahl unendlich ist, wird das Maximum auf 100000 gesetzt. Praktisch gibt es also Maximla 100000
+     * Nutzer pro Gruppe.
+     * @param isMaximumInfinite Gibt an ob es unendlich viele User geben soll
+     * @param userMaximum Das Maximum an Usern, falls es eins gibt
+     * @return Maximum an Usern
+     */
     private Long checkInfiniteUsers(Boolean isMaximumInfinite, Long userMaximum) {
         isMaximumInfinite = isMaximumInfinite != null;
 
